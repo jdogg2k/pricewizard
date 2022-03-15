ef5a33b66fd339e68c3d49d57306b6b1d79981d2
Jordan Rose <jorose@gmail.com>
2022-03-15 09:57:13 -0400

    added auth

--- pricewizard/src/index.js
+++ pricewizard/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +16,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a function here
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
