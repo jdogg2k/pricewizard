4ee1558613a256327f2883328c628cedb8a900c6
jdogg2k <jorose@gmail.com>
2022-03-15 13:14:16 -0400

    Update aws-exports.js

--- pricewizard/src/aws-exports.js
+++ pricewizard/src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_identity_pool_id": "us-east-1:f5903b23-fe17-4525-a221-41300b22a7fd",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_gsUsbpxQa",
+    "aws_user_pools_id": "us-east-1_tjZryRK7P",
     "aws_user_pools_web_client_id": "21djegci01a3054u25k20ephms",
     "oauth": {},
     "aws_cognito_username_attributes": [],
